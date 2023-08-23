adcfb6437d426a4ac26404fe1c4b80d4f3b5a11f
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2023-08-24 01:03:37 +0900

    カード関連処理
    カードを入力エリアに配置できるようにした（仮）
    ドラッグ中に離したカードは初期位置に戻るようにした（仮）

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -1,26 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class CardController : MonoBehaviour
 {
     [SerializeField]
     private Camera _Camera;
+    public float _transformX;
+    public float _transformY;
+    public float _transformZ;
+
+    private bool _isDragging = false;// カードをドラッグしているか
+    private bool _isCollider = false;// カード入力エリアと接触しているか
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _transformX = transform.position.x;
+        _transformY = transform.position.y;
+        _transformZ = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!_isCollider && !_isDragging)// 入力エリアと衝突していない　かつ　ドラッグしていないとき、カードを初期位置に戻す
+        {
+            transform.position = new Vector3(_transformX, _transformY, _transformZ);
+        }
     }
 
     private void OnMouseDrag()
     {
         transform.position = (Vector2)_Camera.ScreenToWorldPoint(Input.mousePosition);// 位置をマウスの位置と同期させる
+        _isDragging = true;
     }
+
+    private void OnMouseUp()
+    {
+        _isDragging = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Debug.Log($"OnTriggerStay2D : {collision.tag}:{collision.name}");
+
+        if (!_isDragging)// ドラッグが終わったときに入力欄と衝突していたら位置を同期する
+        {
+            transform.position = collision.transform.position;
+        }
+
+        _isCollider = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _isCollider = false;
+    }
+
 }

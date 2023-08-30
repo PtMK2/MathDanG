47a29fad59e050b1f70c947a377b949ad598f929
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2023-08-30 14:58:03 +0900

    敵アニメーション他
    ・スライムのアニメーションを作ってみた
    ・動作に影響はなかったが終了時に出る謎のエラー（cannot set the parent）を多分修正した

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,16 +10,25 @@ public class EnemyController : MonoBehaviour
 
     public string enemyName;
     public string enemyDescription;
+
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         enemyName = _enemyData.enrmyName;
         enemyDescription = _enemyData.enemyDescription;
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // random timing to animation
+        if (Random.Range(0, 5000) < 1)
+        {
+            animator.SetTrigger("Jump");
+        }
     }
 }

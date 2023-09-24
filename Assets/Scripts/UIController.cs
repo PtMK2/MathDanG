4ce523e0427d43b550c6cef085923a062a2a1508
PtMK2 <2101036@s.asojuku.ac.jp>
2023-09-24 14:36:46 +0900

    hpバーの色変更

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -24,8 +24,8 @@ public class UIController : MonoBehaviour
     private GameObject EnemyGameObj;
     
 
-    private string enemyName = "hogehoge";// ‚±‚Ì•Ï”‚ÍÁ‚»‚¤‚Æv‚¦‚ÎÁ‚¹‚é
-    private string enemyDescription = "hogehoge piyopiyo";// ‚±‚Ì•Ï”‚ÍÁ‚»‚¤‚Æv‚¦‚ÎÁ‚¹‚é
+    private string enemyName = "hogehoge";// ï¿½ï¿½ï¿½Ì•Ïï¿½ï¿½Íï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ævï¿½ï¿½ï¿½Îï¿½ï¿½ï¿½ï¿½ï¿½
+    private string enemyDescription = "hogehoge piyopiyo";// ï¿½ï¿½ï¿½Ì•Ïï¿½ï¿½Íï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ævï¿½ï¿½ï¿½Îï¿½ï¿½ï¿½ï¿½ï¿½
 
     
 
@@ -51,7 +51,8 @@ public class UIController : MonoBehaviour
             playerHpBarImage.color = Color.yellow;
         } else
         {
-            playerHpBarImage.color = Color.green;
+            //playerHpBarImage.color = Color.green;
+            playerHpBarImage.color = new Color(0f, 0.816f, 0.090f);
         }
 
         if (enemyHpBar.value <= enemyHpBar.maxValue * 0.2)
@@ -64,7 +65,8 @@ public class UIController : MonoBehaviour
         }
         else
         {
-            enemyHpBarImage.color = Color.green;
+            enemyHpBarImage.color = new Color(0f, 0.816f, 0.090f);
+            //enemyHpBarImage.color = Color.green;
         }
 
         if (Input.GetMouseButtonDown(0))
@@ -84,11 +86,11 @@ public class UIController : MonoBehaviour
 
                 EnemyGameObj = hit2D.transform.gameObject;
 
-                // ƒNƒŠƒbƒN‚³‚ê‚½“G‚Ìî•ñæ“¾
+                // ï¿½Nï¿½ï¿½ï¿½bï¿½Nï¿½ï¿½ï¿½ê‚½ï¿½Gï¿½Ìï¿½ï¿½æ“¾
                 enemyName = EnemyGameObj.GetComponent<EnemyController>().enemyName;
                 enemyDescription = EnemyGameObj.GetComponent<EnemyController>().enemyDescription;
 
-                // æ“¾‚µ‚½î•ñ‚ğƒpƒlƒ‹‚É”½‰f‚³‚¹‚é
+                // ï¿½æ“¾ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½pï¿½lï¿½ï¿½ï¿½É”ï¿½ï¿½fï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
                 enemyInfoPanel.transform.Find("EnemyName").GetComponent<TextMeshProUGUI>().text = enemyName;
                 enemyInfoPanel.transform.Find("EnemyDescription").GetComponent<TextMeshProUGUI>().text = enemyDescription;
 
